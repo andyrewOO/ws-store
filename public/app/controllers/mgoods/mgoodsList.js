232e7a033845c5ada2e4839106326a2514bfd10f
andy <andrewgb@qq.com>
2017-10-13 15:57:10 +0800

    init

--- public/app/controllers/mgoods/mgoodsList.js
+++ public/app/controllers/mgoods/mgoodsList.js
@@ -11,7 +11,7 @@ app.controller('mgoodsListCtrl', [ '$rootScope', '$scope', '$modal', 'egfhttp',
 			$scope.obj = {}
 			$rootScope.search = {}
 
-			$scope.submitSearchParam = function() {
+			$scope.submitSearchParam = function(index, pageSize) {
 				var mgoods = {
 						kind:$scope.obj.kind,
 						place:$scope.obj.place,
@@ -19,10 +19,8 @@ app.controller('mgoodsListCtrl', [ '$rootScope', '$scope', '$modal', 'egfhttp',
 						status:$scope.obj.status
 				}
 				var page = {
-						start:$scope.pageNumber,
-		        		size:$scope.pageSize,
-		        		sum:-1,
-		        		result:null
+						start:index,
+		        		size:pageSize
 				}
 				var subParam = {}
 				subParam.mgoods = mgoods
@@ -37,44 +35,21 @@ app.controller('mgoodsListCtrl', [ '$rootScope', '$scope', '$modal', 'egfhttp',
 					$scope.objs = {};
 					if(data.total>0){
 						$scope.objs.total = data.total
-						$scope.objs.colume = data.data
+						$scope.objs.colume = data.data.result
 					}
-					$scope.objs.colume = data.data;
 					$scope.objs.page = {
-						size : 5,
-						index : 1
+						size : data.data.size,
+						index : data.data.start
 					};
+					$scope.objs.total = data.data.sum
 				});
 			}
 			
 			//分页
-			$scope.showPage = (function(from) {
-				var mgoodsP = {
-						kind:$scope.obj.kind,
-						place:$scope.obj.place,
-						description:$scope.obj.description,
-						status:$scope.obj.status
-				}
-				var searchData = {
-						mgoods:mgoodsP
-				}
-				
-				egfhttp.sendHttp(upXmlView, subParam, {
-					"headers" : {"Content-Type" : "application/json"},
-					"method" : "POST"
-				}, function(data) {
-					console.info(data);
-					$scope.objs={}
-					$scope.objs.page={
-							size: 5,
-							index:1
-					}
-					if(data.total>0){
-						console.info(data.total)
-						$scope.objs.colume = data.data;
-					}
-					console.info(scope.content);
-				});
+			$scope.showPage = (function() {
+				var index = $scope.objs.page.index
+				var size = $scope.objs.page.size
+				$scope.submitSearchParam(index, size)
 			})
 
 			// 查看详细
